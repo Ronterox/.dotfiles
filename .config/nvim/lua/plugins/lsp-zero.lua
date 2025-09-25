6bbefc79ba30225e87fdc90fc4ff1922bddae4db
Ricardo <ricarditomontserrat@gmail.com>
2025-09-26 00:13:04 +0200

    Pyright config, may help, may not

--- .config/nvim/lua/plugins/lsp-zero.lua
+++ .config/nvim/lua/plugins/lsp-zero.lua
@@ -104,6 +104,18 @@ return {
                 },
             })
 
+            vim.lsp.config('pyright', {
+                settings = {
+                    python = {
+                        analysis = {
+                            autoSearchPaths = true,
+                            useLibraryCodeForTypes = true,
+                            diagnosticMode = 'openFilesOnly',
+                        },
+                    },
+                },
+            })
+
             vim.lsp.config('pylsp', {
                 capabilities = require('cmp_nvim_lsp').default_capabilities(),
                 settings = {
