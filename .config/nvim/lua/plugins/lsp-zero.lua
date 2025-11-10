1201311d43da238bf28f5e3d4674c7def9a15e71
Ricardo <ricarditomontserrat@gmail.com>
2025-11-10 19:03:05 +0100

    lots of tool simple configuration changes
    M  .config/nvim/lua/plugins/lsp-zero.lua
    M  .local/bin/anx
    M  .local/bin/jkl
    M  .local/bin/sendmail
    M  .local/bin/start-tool
    M  .local/share/backup.log
    M  .newsboat/config

--- .config/nvim/lua/plugins/lsp-zero.lua
+++ .config/nvim/lua/plugins/lsp-zero.lua
@@ -163,7 +163,7 @@ return {
             },
             format_on_save = {
                 -- These options will be passed to conform.format()
-                timeout_ms = 500,
+                timeout_ms = 5000,
                 lsp_format = "fallback",
             },
         },
