2aa41eb388598b0345d2fa209a20bfc99a5322ba
Ricardo <ricarditomontserrat@gmail.com>
2025-11-17 22:34:29 +0100

    simplified urls, added build command for start-tool
    M  .config/nvim/lua/plugins/init.lua
    M  .local/bin/anx
    M  .local/bin/sendmail
    M  .local/bin/start-tool
    M  .newsboat/urls

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,146 +1,144 @@
 return {
-    { 'subnut/nvim-ghost.nvim', enabled = false },
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		keys = {
+			{ "s",         mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+			{ "<leader>s", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+			{ "r",         mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+			{ "R",         mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+			{ "<c-s>",     mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+		},
+	},
 
-    {
-        "folke/flash.nvim",
-        event = "VeryLazy",
-        ---@type Flash.Config
-        opts = {},
-        keys = {
-            { "s",         mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-            { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-            { "r",         mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-            { "R",         mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-            { "<c-s>",     mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-        },
-    },
+	{
+		"S1M0N38/love2d.nvim",
+		cmd = "LoveRun",
+		enabled = true,
+		version = "2.*",
+		opts = {},
+		ft = 'lua',
+		keys = {
+			{ '<leader>v',  ft = 'lua',                          desc = 'LÖVE' },
+			{ '<leader>vv', '<CMD>LoveRun<CR>',                  ft = 'lua',   desc = 'Run LÖVE' },
+			{ '<leader>vs', '<CMD>LoveStop<CR>',                 ft = 'lua',   desc = 'Stop LÖVE' },
+			{ '<F5>',       '<CMD>LoveStop<CR><CMD>LoveRun<CR>', ft = 'lua',   desc = 'Rerun LÖVE' },
+		},
+	},
 
-    {
-        "S1M0N38/love2d.nvim",
-        cmd = "LoveRun",
-        enabled = true,
-        version = "2.*",
-        opts = {},
-        ft = 'lua',
-        keys = {
-            { '<leader>v',  ft = 'lua',                          desc = 'LÖVE' },
-            { '<leader>vv', '<CMD>LoveRun<CR>',                  ft = 'lua',   desc = 'Run LÖVE' },
-            { '<leader>vs', '<CMD>LoveStop<CR>',                 ft = 'lua',   desc = 'Stop LÖVE' },
-            { '<F5>',       '<CMD>LoveStop<CR><CMD>LoveRun<CR>', ft = 'lua',   desc = 'Rerun LÖVE' },
-        },
-    },
+	-- TODO: Organize everything on their own stuff
+	{
+		"monkoose/DoNe",
+		lazy = true,
+		event = { 'BufReadPre *.script' },
+		-- optional configuration
+		config = function()
+			-- as example adding some keybindings
+			vim.keymap.set('n', '<F5>', '<Cmd>DoNe build<CR>')
+			vim.keymap.set('n', '<F6>', '<Cmd>DoNe reload<CR>')
+			--- ...
+		end,
+	},
 
-    -- TODO: Organize everything on their own stuff
-    {
-        "monkoose/DoNe",
-        lazy = true,
-        event = { 'BufReadPre *.script' },
-        -- optional configuration
-        config = function()
-            -- as example adding some keybindings
-            vim.keymap.set('n', '<F5>', '<Cmd>DoNe build<CR>')
-            vim.keymap.set('n', '<F6>', '<Cmd>DoNe reload<CR>')
-            --- ...
-        end,
-    },
+	-- Remote Connection
 
-    -- Remote Connection
+	-- Obligatory
+	-- remote_host     example.com # remote host to connect (must have ssh enabled)
+	-- remote_path     ~/temp/ # remote folder to be synced
+	-- Optional
+	-- remote_user    john # username to connect with
+	-- remote_port    22 # remote ssh port to connect to (default is 22)
+	-- remote_passwd  secret # password to connect with (requires sshpass) (needed if not using ssh-keys)
+	-- local_path    /home/ken/temp/vuetest/ # local folder to be synced (defaults to folder of .vim-arsync)
+	-- ignore_path     ["build/","test/"] # list of ingored files/folders
+	-- ignore_dotfiles 1 # set to 1 to not sync dotfiles (e.g. .vim-arsync)
+	-- auto_sync_up    0 # set to 1 for activating automatic upload syncing on file save
+	-- remote_or_local remote # set to 'local' if you want to perform syncing locally
+	-- sleep_before_sync 0 # set to x seconds if you want to sleep before sync (like compiling a file before syncing)
+	{
+		'kenn7/vim-arsync',
+		cond = vim.fn.filereadable('.vim-arsync') == 1,
+		dependencies = { 'prabirshrestha/async.vim' },
+	},
 
-    -- Obligatory
-    -- remote_host     example.com # remote host to connect (must have ssh enabled)
-    -- remote_path     ~/temp/ # remote folder to be synced
-    -- Optional
-    -- remote_user    john # username to connect with
-    -- remote_port    22 # remote ssh port to connect to (default is 22)
-    -- remote_passwd  secret # password to connect with (requires sshpass) (needed if not using ssh-keys)
-    -- local_path    /home/ken/temp/vuetest/ # local folder to be synced (defaults to folder of .vim-arsync)
-    -- ignore_path     ["build/","test/"] # list of ingored files/folders
-    -- ignore_dotfiles 1 # set to 1 to not sync dotfiles (e.g. .vim-arsync)
-    -- auto_sync_up    0 # set to 1 for activating automatic upload syncing on file save
-    -- remote_or_local remote # set to 'local' if you want to perform syncing locally
-    -- sleep_before_sync 0 # set to x seconds if you want to sleep before sync (like compiling a file before syncing)
-    {
-        'kenn7/vim-arsync',
-        cond = vim.fn.filereadable('.vim-arsync') == 1,
-        dependencies = { 'prabirshrestha/async.vim' },
-    },
+	{ 'gpanders/nvim-parinfer' },
 
-    { 'gpanders/nvim-parinfer' },
+	-- Apple Pkl Generate Config Files
+	{
+		'apple/pkl-neovim',
+		as = 'pkl',
+		event = {
+			'BufReadPre *.pkl',
+			'BufReadPre *.pcf',
+			'BufReadPre PklProject',
+		},
+	},
 
-    -- Apple Pkl Generate Config Files
-    {
-        'apple/pkl-neovim',
-        as = 'pkl',
-        event = {
-            'BufReadPre *.pkl',
-            'BufReadPre *.pcf',
-            'BufReadPre PklProject',
-        },
-    },
+	-- Discord Presence
+	{ 'andweeb/presence.nvim', event = "InsertCharPre" },
 
-    -- Discord Presence
-    { 'andweeb/presence.nvim', event = "InsertCharPre" },
-
-    -- Sonic Pi
-    {
-        'magicmonty/sonicpi.nvim',
-        dependencies = { 'hrsh7th/nvim-cmp', 'kyazdani42/nvim-web-devicons' },
-        config = function()
-            local sonicpi = require('sonicpi')
-            sonicpi.setup({ lsp_diagnostics = true, })
-        end,
-        ft = 'sonicpi'
-    },
+	-- Sonic Pi
+	{
+		'magicmonty/sonicpi.nvim',
+		dependencies = { 'hrsh7th/nvim-cmp', 'kyazdani42/nvim-web-devicons' },
+		config = function()
+			local sonicpi = require('sonicpi')
+			sonicpi.setup({ lsp_diagnostics = true, })
+		end,
+		ft = 'sonicpi'
+	},
 
 
-    -- Beautify Vim
-    {
-        'Mofiqul/vscode.nvim',
-        opts = {
-            transparent = true,
-            color_overrides = {
-                vscYellow = '#e46c8e',
-                vscOrange = '#DCDCAA',
-                vscBlueGreen = '#56C5D6'
-            },
-            group_overrides = {
-                LineNr                = { fg = '#DCDCAA' },
-                DiagnosticUnnecessary = { fg = '#5a5a5a' },
-            }
-        }
-    },
+	-- Beautify Vim
+	{
+		'Mofiqul/vscode.nvim',
+		opts = {
+			transparent = true,
+			color_overrides = {
+				vscYellow = '#e46c8e',
+				vscOrange = '#DCDCAA',
+				vscBlueGreen = '#56C5D6'
+			},
+			group_overrides = {
+				LineNr                = { fg = '#DCDCAA' },
+				DiagnosticUnnecessary = { fg = '#5a5a5a' },
+			}
+		}
+	},
 
-    -- Smart basics of vim
-    { 'tpope/vim-surround',    keys = { "cs", "ds", "yss" } },
+	-- Smart basics of vim
+	{ 'tpope/vim-surround',  keys = { "cs", "ds", "yss" } },
 
-    -- Code Highlighting
-    { 'chaimleib/vim-renpy',   ft = 'renpy' },
-    -- https://github.com/AVagueNumberOfHumans/renpyls
+	-- Code Highlighting
+	{ 'chaimleib/vim-renpy', ft = 'renpy' },
+	-- https://github.com/AVagueNumberOfHumans/renpyls
 
-    { 'fladson/vim-kitty',     ft = 'kitty' },
-    {
-        'norcalli/nvim-colorizer.lua',
-        event = { "VeryLazy", "BufReadPre" },
-        config = function() require 'colorizer'.setup() end
-    },
-    {
-        'lukas-reineke/indent-blankline.nvim',
-        event = { "VeryLazy", "BufReadPre" },
-        config = function() require 'ibl'.setup() end
-    },
+	{ 'fladson/vim-kitty',   ft = 'kitty' },
+	{
+		'norcalli/nvim-colorizer.lua',
+		event = { "VeryLazy", "BufReadPre" },
+		config = function() require 'colorizer'.setup() end
+	},
+	{
+		'lukas-reineke/indent-blankline.nvim',
+		event = { "VeryLazy", "BufReadPre" },
+		config = function() require 'ibl'.setup() end
+	},
 
-    -- Tracking Tools
-    { 'wakatime/vim-wakatime' },
+	-- Tracking Tools
+	{ 'wakatime/vim-wakatime' },
 
-    -- Fast Typing
-    { "windwp/nvim-autopairs", event = "InsertCharPre",               config = true },
-    { 'numToStr/Comment.nvim', keys = { "gc", { "gc", mode = "v" } }, config = true },
+	-- Fast Typing
+	{ "windwp/nvim-autopairs", event = "InsertCharPre",               config = true },
+	{ 'numToStr/Comment.nvim', keys = { "gc", { "gc", mode = "v" } }, config = true },
 
-    -- File Previewer
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
-    },
+	-- File Previewer
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function() vim.fn["mkdp#util#install"]() end,
+	},
 }
