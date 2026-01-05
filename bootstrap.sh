bde69e2419f288ba99c84037296a4b43dd76feb9
Ricardo <ricarditomontserrat@gmail.com>
2026-01-06 00:19:44 +0100

    decent bootstrap for nix on arch
    M  bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
+# TODO: Setup the disks
+
+# TODO: Setup i3wm
+
 set -e
 
 pacman -Syu --noconfirm neovim sudo nix git base-devel
 
-useradd -m -G wheel rontero
-passwd rontero
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
+nix-channel --update
 
 # Create a new file that enables the wheel group
 echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/wheel
 
+useradd -m -G wheel rontero
+passwd rontero
+
 # - tells to login
-su - rontero -c "git clone -b linux https://github.com/Ronterox/.dotfiles.git"
+su - rontero -c <<EOF
+	git clone -b linux https://github.com/Ronterox/.dotfiles.git
+	cd .dotfiles
+	nix-env -i stow
+	stow .
+EOF
 
 su - rontero
