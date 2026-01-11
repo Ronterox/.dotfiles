33648fe63ad28eda4f298af97bda89b276f650a3
Ricardo <ricarditomontserrat@gmail.com>
2026-01-11 07:45:31 +0100

    Can activate code

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,41 +8,61 @@ HRDW_PKGS="linux-firmware intel-ucode nvidia"
 BOOT_PKGS="refind efibootmgr"
 BASE_PKGS="$CORE_PKGS $HRDW_PKGS $BOOT_PKGS"
 
+EDIT_SETUP=false
 HRDW_SETUP=false
 SFWR_SETUP=false
 
-# Check if neovim and git are installed
-if ! command -v nvim &> /dev/null || ! command -v git &> /dev/null; then
-	mount -o remount,size=$COWSPACE_SIZE /run/archiso/cowspace
-	pacman -Syu --noconfirm neovim git
-	git clone https://github.com/NvChad/starter ~/.config/nvim
-	exit 0
-fi
+# Hardware Configuration
+FDISK=false
+MKFS=false
+MOUNT=false
+
+PACSTRAP=false
+REFIND=false
 
 set -e
 
+# Check if neovim and git are installed
+if [ "$EDIT_SETUP" = true ]; then
+	if ! command -v nvim &> /dev/null || ! command -v git &> /dev/null; then
+		mount -o remount,size=$COWSPACE_SIZE /run/archiso/cowspace
+		pacman -Syu --noconfirm neovim git
+		git clone https://github.com/NvChad/starter ~/.config/nvim
+		exit 0
+	fi
+fi
+
 # Hardware Setup
 if [ "$HRDW_SETUP" = true ]; then
-	fdisk -l && lsblk
-	# n,1,default,+512M (if not exists),t,1(EFI)
-	# n,2,default,default,t,23(root x86_64)
-	# p (shows partitions, if not gpt do g)
-	fdisk $DISK
+	if [ "$FDISK" = true ]; then
+		fdisk -l && lsblk
+		# n,1,default,+512M (if not exists),t,1(EFI)
+		# n,2,default,default,t,23(root x86_64)
+		# p (shows partitions, if not gpt do g)
+		fdisk $DISK
+	fi
 
-	# if not exists
-	mkfs.fat -F32 "$DISK"1
-	mkfs.ext4 "$DISK"2
+	if [ "$MKFS" = true ]; then
+		# if not exists
+		mkfs.fat -F32 "$DISK"1
+		mkfs.ext4 "$DISK"2
+	fi
 
-	mount --mkdir "$DISK"1 /mnt/boot
-	mount "$DISK"2 /mnt
+	if [ "$MOUNT" = true ]; then
+		mount --mkdir "$DISK"1 /mnt/boot
+		mount "$DISK"2 /mnt
+	fi
 
-	# Speed for downloading packages
-	reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-	pacstrap -K /mnt/ "$BASE_PKGS"
+	if [ "$PACSTRAP" = true ]; then
+		# Speed for downloading packages
+		reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+		pacstrap -K /mnt/ "$BASE_PKGS"
+	fi
 
-	arch-chroot /mnt/
-
-	refind-install
+	if [ "$REFIND" = true ]; then
+		arch-chroot /mnt/
+		refind-install
+	fi
 fi
 
 # Software Setup
