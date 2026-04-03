d2aa11176edc7a04f8cb4d8a7d635eb818743a8d
Ricardo <ricarditomontserrat@gmail.com>
2026-04-04 01:31:11 +0200

    fix(emacs): config new version errors and warnings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-;; Set up package.el to work with MELPA
+;; Set up package.el to work with MELPA  -*- lexical-binding: t; -*-
 
 (require 'package)
 (add-to-list 'package-archives
@@ -6,7 +6,7 @@
 (setq package-check-signature nil)
 (package-initialize)
 
-(unless package--archive-contents
+(unless (assoc 'melpa package-archives)
   (package-refresh-contents))
 
 ;; Magit
@@ -37,12 +37,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages '(cmake-mode evil evil-collection magit)))
-(custom-set-face
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;; Evil Mode
 (define-key evil-insert-state-map (kbd "C-c C-c") 'evil-normal-state)
