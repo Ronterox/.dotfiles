3c338705b777cb50374e2a59b9ec669ebfda050f
Ricardo <ricarditomontserrat@gmail.com>
2026-04-11 16:57:07 +0200

    feat: added opencode-nvim plugin
    M  .config/nvim/lazy-lock.json
    M  .config/nvim/lua/plugins/ai.lua
    M  todo.org
    M  todo.org_archive

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,129 +1,4 @@
----@param dir string
----@return string?
-local function findNewestFile(dir)
-	local newest_file = nil
-	local newest_time = 0
-
-	for file in vim.fs.dir(dir) do
-		local full_path = dir .. "/" .. file
-		local stat = vim.uv.fs_stat(full_path)
-
-		if stat and stat.type == "file" and stat.mtime.sec > newest_time then
-			newest_time = stat.mtime.sec
-			newest_file = full_path
-		end
-	end
-
-	return newest_file
-end
-
----@type string?
-local file = ""
-
 return {
-	{
-		"ThePrimeagen/99",
-		config = function()
-			local _99 = require("99")
-
-			-- For logging that is to a file if you wish to trace through requests
-			-- for reporting bugs, i would not rely on this, but instead the provided
-			-- logging mechanisms within 99.  This is for more debugging purposes
-			local cwd = vim.uv.cwd()
-			local basename = vim.fs.basename(cwd)
-			_99.setup({
-				model = "opencode/big-pickle",
-
-				logger = {
-					level = _99.DEBUG,
-					path = "/tmp/" .. basename .. ".99.debug",
-					print_on_error = true,
-				},
-
-				--- A new feature that is centered around tags
-				completion = {
-					--- Defaults to .cursor/rules
-					-- I am going to disable these until i understand the
-					-- problem better.  Inside of cursor rules there is also
-					-- application rules, which means i need to apply these
-					-- differently
-					-- cursor_rules = "<custom path to cursor rules>"
-
-					--- A list of folders where you have your own SKILL.md
-					--- Expected format:
-					--- /path/to/dir/<skill_name>/SKILL.md
-					---
-					--- Example:
-					--- Input Path:
-					--- "scratch/custom_rules/"
-					---
-					--- Output Rules:
-					--- {path = "scratch/custom_rules/vim/SKILL.md", name = "vim"},
-					--- ... the other rules in that dir ...
-					---
-					custom_rules = { "scratch/custom_rules/" },
-
-					--- What autocomplete do you use.  We currently only
-					--- support cmp right now
-					source = "cmp",
-				}
-
-				--- WARNING: if you change cwd then this is likely broken
-				--- ill likely fix this in a later change
-				---
-				--- md_files is a list of files to look for and auto add based on the location
-				--- of the originating request.  That means if you are at /foo/bar/baz.lua
-				--- the system will automagically look for:
-				--- /foo/bar/AGENT.md
-				--- /foo/AGENT.md
-				--- assuming that /foo is project root (based on cwd)
-				-- md_files = { "AGENT.md" },
-			})
-
-			local call99 = function(fn)
-				return function()
-					vim.fn.mkdir("./tmp", "p")
-					fn()
-				end
-			end
-
-			-- Create your own short cuts for the different types of actions
-			vim.keymap.set("n", "<leader>cf", call99(_99.fill_in_function), { desc = "Fill in function" })
-			vim.keymap.set("n", "<leader>cc", call99(_99.fill_in_function_prompt), { desc = "Fill in with prompt" })
-
-			local function readLatestOutput()
-				local latest = findNewestFile("./tmp")
-				if latest and latest ~= file then
-					local mode = vim.fn.mode()
-					if mode == "v" or mode == "V" then vim.cmd("normal! _d") end
-					vim.cmd.read(latest)
-					file = latest
-					return
-				end
-				print("No new file")
-			end
-			vim.keymap.set("n", "<leader>cp", readLatestOutput, { desc = "Pastes the latest prompt output" })
-			vim.keymap.set("v", "<leader>cp", readLatestOutput, { desc = "Pastes the latest prompt output" })
-			vim.keymap.set("n", "<leader>cap", function()
-				vim.cmd("normal! vap")
-				readLatestOutput()
-			end, { desc = "Pastes the latest prompt output around the paragraph" })
-
-			-- take extra note that i have visual selection only in v mode
-			-- technically whatever your last visual selection is, will be used
-			-- so i have this set to visual mode so i dont screw up and use an
-			-- old visual selection
-			--
-			-- likely ill add a mode check and assert on required visual mode
-			-- so just prepare for it now
-			vim.keymap.set("v", "<leader>cf", call99(_99.visual), { desc = "Visual fill" })
-			vim.keymap.set("v", "<leader>cc", call99(_99.visual_prompt), { desc = "Visual prompt" })
-
-			--- if you have a request you dont want to make any changes, just cancel it
-			vim.keymap.set("n", "<leader>cz", _99.stop_all_requests, { desc = "Cancel request" })
-			vim.keymap.set("v", "<leader>cz", _99.stop_all_requests, { desc = "Cancel request" })
-		end,
-	},
 	{
 		'supermaven-inc/supermaven-nvim',
 		config = function()
@@ -328,5 +203,63 @@ return {
 				end)
 			end, { desc = "Run Code Missing API Generator" })
 		end
-		}
+	},
+	{
+		"nickjvandyke/opencode.nvim",
+		version = "*", -- Latest stable release
+		dependencies = {
+			{
+				-- `snacks.nvim` integration is recommended, but optional
+				---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
+				"folke/snacks.nvim",
+				optional = true,
+				opts = {
+					input = {}, -- Enhances `ask()`
+					picker = { -- Enhances `select()`
+						actions = {
+							opencode_send = function(...) return require("opencode").snacks_picker_send(...) end,
+						},
+						win = {
+							input = {
+								keys = {
+									["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		config = function()
+			---@type opencode.Opts
+			vim.g.opencode_opts = {
+				-- Your configuration, if any; goto definition on the type or field for details
+			}
+
+			vim.o.autoread = true -- Required for `opts.events.reload`
+
+			-- Recommended/example keymaps
+			vim.keymap.set({ "n", "x" }, "<leader>ca",
+				function() require("opencode").ask("@this: ", { submit = true }) end,
+				{ desc = "Ask opencode…" })
+			vim.keymap.set({ "n", "x" }, "<leader>cx", function() require("opencode").select() end,
+				{ desc = "Execute opencode action…" })
+			vim.keymap.set({ "n", "t" }, "<leader>ct", function() require("opencode").toggle() end,
+				{ desc = "Toggle opencode" })
+
+			vim.keymap.set({ "n", "x" }, "go", function() return require("opencode").operator("@this ") end,
+				{ desc = "Add range to opencode", expr = true })
+			vim.keymap.set("n", "goo", function() return require("opencode").operator("@this ") .. "_" end,
+				{ desc = "Add line to opencode", expr = true })
+
+			vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,
+				{ desc = "Scroll opencode up" })
+			vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end,
+				{ desc = "Scroll opencode down" })
+
+			-- You may want these if you use the opinionated `<C-a>` and `<C-x>` keymaps above — otherwise consider `<leader>o…` (and remove terminal mode from the `toggle` keymap)
+			-- vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
+			-- vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
+		end,
 	}
+}
